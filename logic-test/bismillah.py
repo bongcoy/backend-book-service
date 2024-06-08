e201888107aaf58b68bd33b5889b7fa07e7d8fb4
bongcoy <mpascaldewantara@gmail.com>
2024-06-08 10:55:18 +0700

    update default value

--- logic-test/bismillah.py
+++ logic-test/bismillah.py
@@ -1,5 +1,7 @@
 def no1():
-    s = input()
+    s = input("1. masukkan string (klik enter untuk nilai default): ")
+    if len(s) == 0:
+        s = "NEGIE1"
     # get number substring
     number = ''.join([i for i in s if i.isdigit()])
     # get alphabet substring
@@ -11,14 +13,20 @@ def no1():
     print(result)
 
 def no2():
-    s = input()
+    s = input("2. masukkan string (klik enter untuk nilai default): ")
+    if len(s) == 0:
+        s = "Saya sangat senang mengerjakan soal algoritma"
     # get longest word in sentence
     longest_word = max(s.split(), key=len)
     print(f"{longest_word}: {len(longest_word)} character")
 
 def no3():
-    arr1 = input().split()
-    arr2 = input().split()
+    print("3. masukkan dua array: ")
+    arr1 = input("INPUT (klik enter untuk nilai default) = ").split()
+    arr2 = input("QUERY (klik enter untuk nilai default) = ").split()
+    if len(arr1) == 0 or len(arr2) == 0:
+        arr1 = ['xc', 'dz', 'bbb', 'dz'] 
+        arr2 = ['bbb', 'ac', 'dz']
     # set arr2 unique element order by first appearance
     arr2 = list(dict.fromkeys(arr2))
     # create dictionary to store element count
@@ -36,8 +44,13 @@ def no3():
 
 def no4():
     # input nxn matrix
-    n = int(input("masukkan ukuran matriks nxn: "))
-    matrix = [list(map(int, input(f"masukkan {n} bilangan untuk baris {i}: ").split())) for i in range(n)]
+    n = int(input("4. masukkan ukuran matriks nxn (klik enter untuk nilai default): "))
+    matrix = []
+    if n == 0:
+        n = 3
+        matrix = [[1, 2, 0], [4, 5, 6], [7, 8, 9]]
+    else:
+        matrix = [list(map(int, input(f"masukkan {n} bilangan untuk baris {i}: ").split())) for i in range(n)]
     # get sum of diagonal element
     sum1 = sum(matrix[i][i] for i in range(n))
     # get sum of anti-diagonal element
@@ -48,10 +61,11 @@ def no4():
     print(result)
 
 def main():
-    # no1()
+    # Uncomment salah satu fungsi dibawah ini untuk menjalankan soal yang diinginkan
+    no1()
     # no2()
     # no3()
-    no4()
+    # no4()
 
 if __name__ == '__main__':
     main()
