69b9f10d3aab8c4920b8db09c8508e03ee4da366
bongcoy <mpascaldewantara@gmail.com>
2024-06-08 20:31:15 +0700

    add 3 entities resource

--- backend-book-service/src/app.module.ts
+++ backend-book-service/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MemberModule } from './member/member.module';
+import { BorrowingsModule } from './borrowings/borrowings.module';
+import { BooksModule } from './books/books.module';
+import { MembersModule } from './members/members.module';
+import { MembersModule } from './members/members.module';
+import { MemberModule } from './member/member.module';
 
 @Module({
-  imports: [],
+  imports: [MemberModule, MembersModule, BooksModule, BorrowingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
