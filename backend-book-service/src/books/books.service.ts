eb1c2e9478f146fdfe7b558fb5374f18576c18f0
bongcoy <mpascaldewantara@gmail.com>
2024-06-09 02:24:38 +0700

    add prisma services

--- backend-book-service/src/books/books.service.ts
+++ backend-book-service/src/books/books.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class BooksService {
+  constructor(private prisma: PrismaService) {}
+
   create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+    return this.prisma.book.create({ data: createBookDto });
   }
 
   findAll() {
-    return `This action returns all books`;
+    return this.prisma.book.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} book`;
+  findOne(code: string) {
+    return this.prisma.book.findUnique({ where: { code } });
   }
 
-  update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
+  update(code: string, updateBookDto: UpdateBookDto) {
+    return this.prisma.book.update({ where: { code }, data: updateBookDto });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} book`;
+  remove(code: string) {
+    return this.prisma.book.delete({ where: { code } });
   }
 }
