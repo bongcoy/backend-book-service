1c9a519988132a140c9b3ac75667067fdc9a28af
bongcoy <mpascaldewantara@gmail.com>
2024-06-09 00:06:59 +0700

    update modul providers

--- backend-book-service/src/app.module.ts
+++ backend-book-service/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
-import { BorrowingsModule } from './borrowings/borrowings.module';
 import { BooksModule } from './books/books.module';
+import { BorrowingsModule } from './borrowings/borrowings.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [MembersModule, MembersModule, BooksModule, BorrowingsModule],
+  imports: [MembersModule, BooksModule, BorrowingsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
