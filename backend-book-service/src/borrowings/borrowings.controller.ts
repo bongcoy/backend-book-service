078adad74d6d4052452f18611f99c3adbf50b147
bongcoy <mpascaldewantara@gmail.com>
2024-06-09 11:00:37 +0700

    update id to code

--- backend-book-service/src/borrowings/borrowings.controller.ts
+++ backend-book-service/src/borrowings/borrowings.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { BorrowingsService } from './borrowings.service';
 import { CreateBorrowingDto } from './dto/create-borrowing.dto';
 import { UpdateBorrowingDto } from './dto/update-borrowing.dto';
@@ -18,17 +26,20 @@ export class BorrowingsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.borrowingsService.findOne(+id);
+  findOne(@Param('id') code: string) {
+    return this.borrowingsService.findOne(+code);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBorrowingDto: UpdateBorrowingDto) {
-    return this.borrowingsService.update(+id, updateBorrowingDto);
+  update(
+    @Param('id') code: string,
+    @Body() updateBorrowingDto: UpdateBorrowingDto,
+  ) {
+    return this.borrowingsService.update(+code, updateBorrowingDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.borrowingsService.remove(+id);
+  remove(@Param('id') code: string) {
+    return this.borrowingsService.remove(+code);
   }
 }
